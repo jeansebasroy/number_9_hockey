399cc39085838d4345302d0da79eddc7adbd5ab1
J.S. Roy <jroy@chicagobooth.edu>
2014-07-31 11:18:07 -0500

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 describe "User pages" do
 
+  let(:base_title) { "#9 Hockey" }
+  # need to create a 'helper' file to define this for both "Static pages" & "User pages"
+
 	subject { page }
 
   	describe "signup page" do
     	before { visit signup_path }
 
     	it { should have_content('Sign Up') }
-    	it { should have_title(full_title('Sign Up')) }
+      it { should have_title("#{base_title} | Sign Up") }
+    	#it { should have_title(full_title('Sign Up')) }
   	end
 
 	describe "sign up" do
@@ -18,23 +22,40 @@ describe "User pages" do
     	let(:submit) { "Submit" }
 
     	describe "with invalid information" do
+
       		it "should not create a user" do
         		expect { click_button submit }.not_to change(User, :count)
       		end
+
+          describe "after submission" do
+            before { click_button submit }
+
+            it { should have_title("Sign Up") }
+            it { should have_content('error') }
+          end
+
     	end
 
     	describe "with valid information" do
-      		before do
-        		fill_in "First Name",   with: "Example"
-        		fill_in "Last Name", 	with:  "User"
-        		fill_in "Email",        with: "user@example.com"
-        		fill_in "Password",     with: "foobar"
-        		fill_in "Confirmation", with: "foobar"
-      		end
+      	before do
+        	fill_in "First Name",    with: "Example"
+        	fill_in "Last Name",     with: "User"
+        	fill_in "Email",         with: "user@example.com"
+        	fill_in "Password",      with: "foobar"
+        	fill_in "Confirmation",  with: "foobar"
+      	end
 
-      		it "should create a user" do
-        	expect { click_button submit }.to change(User, :count).by(1)
-      		end
+      	it "should create a user" do
+         	expect { click_button submit }.to change(User, :count).by(1)
+      	end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by(email: "user@example.com") }
+
+          it { should have_title(user.first_name) }
+          it { should have_selector('div.alert.alert-success', text: "Welcome") }
+        end
     	end
   	end
 
