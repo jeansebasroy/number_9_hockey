bc483e6057dcbe43f4ad93ef66443c703226ad0c
J.S. Roy <jroy@chicagobooth.edu>
2014-08-13 14:02:06 -0500

    include some Invitation class validations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,12 +1,13 @@
 class Invitation < ActiveRecord::Base
-	#validates :code, presence: true,
-	#			uniqueness: { case_sensitive: true },
+	validates :code, presence: true,
+				uniqueness: true
+	#			{ case_sensitive: true },
 				#length: 8
-	#validates :expiration_date, presence: true
+	validates :expiration_date, presence: true
 
-  #def Invitation.new_invitation_code
+  def Invitation.new_invitation_code
     #get a function that generates a manageable invitation_code
-  #  SecureRandom.urlsafe_base64
-  #end	
+    SecureRandom.urlsafe_base64
+  end	
 
 end
