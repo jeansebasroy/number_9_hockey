2b31c3625aa18c4dd34eee04edd629798d1a6689
J.S. Roy <jroy@chicagobooth.edu>
2014-08-13 14:23:34 -0500

    check unique code against database

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,17 +5,16 @@ class InvitationsController < ApplicationController
   end
 
   def verify_invitation
-  	#verify that the submitted invitation_code is valid
-  		#is in database, has not expired, & has not been used
-  	if 1 == 2
+    valid_code = Invitation.find_by(code: params[:invitations][:invitation_code])
+    #need to verify that the code has not expired & has not been used before
+  	if  valid_code
   		#set the use_date field in the invitations model
   		#allow user to go to signup page
   		redirect_to "/signup"
   	else
   		flash[:error] = "The Invitation Code submitted did not match our records.  Try again."
   		redirect_to "/signin"
-  		#is this pointing to 'new' in the invitations_controller
-  		#or 'new' for the sessions_controller?
+
   	end
   end
 end
