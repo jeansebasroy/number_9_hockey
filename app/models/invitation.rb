7b01e116d93ee731a2b30e21157041975bf3a19b
J.S. Roy <jroy@chicagobooth.edu>
2014-08-13 13:59:10 -0500

    simplified Invitation class

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,8 +1,8 @@
 class Invitation < ActiveRecord::Base
-	validates :code, presence: true,
-				uniqueness: { case_sensitive: true },
+	#validates :code, presence: true,
+	#			uniqueness: { case_sensitive: true },
 				#length: 8
-	validates :expiration_date, presence: true
+	#validates :expiration_date, presence: true
 
   #def Invitation.new_invitation_code
     #get a function that generates a manageable invitation_code
