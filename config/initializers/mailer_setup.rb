55b8b15a5b019b7b70d18201ae80d5060cc1f831
J.S. Roy <jroy@chicagobooth.edu>
2014-09-18 11:48:17 -0500

    finish actionmailer setup

--- config/initializers/mailer_setup.rb
+++ config/initializers/mailer_setup.rb
@@ -8,3 +8,4 @@ ActionMailer::Base.smtp_settings = {
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
