99fbad12786db7ab934163307c62f920cdd2d0e9
J.S. Roy <jroy@chicagobooth.edu>
2014-09-18 10:52:57 -0500

    actionmailer port 587, authentication plain, tls true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address => "p3plcpnl0595.prod.phx3.secureserver.net",
-    :port => 25,
+    :port => 587,
     :domain => "www.number9hockey.com",
     :user_name => "support@number9hockey.com",
     :password => "GordieHowe17",
