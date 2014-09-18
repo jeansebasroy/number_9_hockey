6b4e4a59eaa7fcefbe722978db8ca935f809fa5a
J.S. Roy <jroy@chicagobooth.edu>
2014-09-18 09:41:19 -0500

    match Gulch2 app email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,19 @@ Rails.application.configure do
 
   #config.assets.debug = false
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000' } 
 
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.smtp_settings = {
+  #  address:              'p3plcpnl0595.prod.phx3.secureserver.net',
+  #  port:                 465,
+  #  domain:               'www.number9hockey.com',
+  #  user_name:            'support@number9hockey.com',
+  #  password:             'GordieHowe17',
+  #  authentication:       :login,
+  #  enable_starttls_auto: true  
+  #  }
 end
